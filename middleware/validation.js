44b509b33c1289aa928bb5e75c272192452aa586
Jormaedes Luís <jormahed@gmail.com>
2026-09-08 10:53:52 +0100

    feat: add validation middleware for user signup

---
+++ middleware/validation.js
@@ -0,0 +1,42 @@
+import { body, validationResult } from "express-validator";
+
+// firstname, lastname, username, password
+const validateSignup = [
+	// Campo firstname
+	body('firstname')
+		.trim()
+		.notEmpty()
+		.withMessage('O primeiro nome é obrigatório.')
+		.escape(),
+
+	// Campo lastname
+	body('lastname')
+		.trim()
+		.notEmpty()
+		.withMessage('O último nome é obrigatório.')
+		.escape(),
+
+	// Campo username
+	body('username')
+		.trim()
+		.notEmpty()
+		.withMessage('O nome de utilizador é obrigatório.')
+		.isLength({ min: 3 })
+		.withMessage('O nome de utilizador deve ter pelo menos 3 caracteres.')
+		.escape(),
+
+	// Campo password
+	body('password')
+		.notEmpty()
+		.withMessage('A palavra-passe é obrigatória.')
+		.isLength({ min: 5 })
+		.withMessage('A palavra-passe deve ter pelo menos 5 caracteres.')
+];
+
+export const handleValidationErrors = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  next();
+};
