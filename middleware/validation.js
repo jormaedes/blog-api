75b104e1e00ed8f522f65697e8f3683b25eae280
Jormaedes Luís <jormahed@gmail.com>
2026-09-08 11:19:50 +0100

    refactor: clean up validation code and improve readability

--- middleware/validation.js
+++ middleware/validation.js
@@ -2,21 +2,18 @@ import { body, validationResult } from "express-validator";
 
 // firstname, lastname, username, password
 export const validateSignup = [
-	// Campo firstname
 	body('firstname')
 		.trim()
 		.notEmpty()
 		.withMessage('O primeiro nome é obrigatório.')
 		.escape(),
 
-	// Campo lastname
 	body('lastname')
 		.trim()
 		.notEmpty()
 		.withMessage('O último nome é obrigatório.')
 		.escape(),
 
-	// Campo username
 	body('username')
 		.trim()
 		.notEmpty()
@@ -25,7 +22,6 @@ export const validateSignup = [
 		.withMessage('O nome de utilizador deve ter pelo menos 3 caracteres.')
 		.escape(),
 
-	// Campo password
 	body('password')
 		.notEmpty()
 		.withMessage('A palavra-passe é obrigatória.')
@@ -33,10 +29,23 @@ export const validateSignup = [
 		.withMessage('A palavra-passe deve ter pelo menos 5 caracteres.')
 ];
 
+// username, password
+export const validateLogin = [
+	body('username')
+		.trim()
+		.notEmpty()
+		.withMessage('O nome de utilizador é obrigatório.')
+		.escape(),
+
+	body('password')
+		.notEmpty()
+		.withMessage('A palavra-passe é obrigatória.'),
+];
+
 export const handleValidationErrors = (req, res, next) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-  next();
+	const errors = validationResult(req);
+	if (!errors.isEmpty()) {
+		return res.status(400).json({ errors: errors.array() });
+	}
+	next();
 };
