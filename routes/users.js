490fe92943ee7b12ff96bdfd27fff6c27ca1e151
Jormaedes Luís <jormahed@gmail.com>
2026-09-11 19:36:21 +0100

    feat: add endpoint to retrieve all users with authentication

--- routes/users.js
+++ routes/users.js
@@ -5,6 +5,27 @@ import isAuth from "../middleware/isAuth.js";
 
 const userRouter = Router();
 
+// GET /users
+userRouter.get('/', isAuth, async (req, res) => {
+	try {
+		const users = await prisma.user.findMany({
+			select: {
+				id: true,
+				firstName: true,
+				lastName: true,
+				username: true,
+				userType: true,
+			},
+		});
+
+		res.json(users);
+	} catch (error) {
+		res.status(500).json({
+			message: 'Internal server error',
+		});
+	}
+});
+
 // GET /users/me
 userRouter.get('/me', isAuth, async (req, res) => {
 	try {
